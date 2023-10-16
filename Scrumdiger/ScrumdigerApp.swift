3c8198800fc51f0417514775200a8f125b3a77bf
withseon <dlstjsvv@gmail.com>
2023-10-16 21:50:32 +0900

    Initial Commit

---
+++ Scrumdiger/ScrumdigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdigerApp.swift
+//  Scrumdiger
+//
+//  Created by 정인선 on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

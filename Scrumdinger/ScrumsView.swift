52b5ed65bc4803c849d603f48fe51c351c989431
withseon <dlstjsvv@gmail.com>
2023-10-30 23:32:01 +0900

    ScrumsView

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdiger
+//
+//  Created by 정인선 on 10/30/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}

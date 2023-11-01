7137acf0a876fe9f7e69730d03bbd9523f573629
withseon <dlstjsvv@gmail.com>
2023-11-01 23:56:05 +0900

    DailyScrum 수정

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -9,7 +9,9 @@ import Foundation
 
 
 // 데일리 스크럼 데이터 샘플 생성
-struct DailyScrum{  // 데이터를 가져다 쓰기 위해 구조체로 생성
+// 데이터를 가져다 쓰기 위해 구조체로 생성
+struct DailyScrum: Identifiable{    // Identifiable 프로토콜을 채택하여, 식별자로 사용되는 ID 프로퍼티 생성
+    let id: UUID()
     var title: String
     var attendees: [String]
     var IengthInMinutes: Int
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -5,14 +5,27 @@
 //  Created by 정인선 on 10/30/23.
 //
 
+/**
+ List View을 이용
+ 카드뷰(일별 스크럼에 대한 요약)를 목록으로 만들어 볼 수 있게 함
+ 
+ DailyScrum를 identifible 데이터로 만들어서 List에서 동적으로 뷰를 생성하게 함
+ 그런데, title를 키로 identify하려고 했으나, 유저가 같은 title을 작성할 수 있는 경우를 대비하여
+ DailyScrum의 프로퍼티 id를 UUID()로 생성해줌
+ */
+
 import SwiftUI
 
 struct ScrumsView: View {
+    let scrums: [DailyScrum]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(scrums, id: \.title){ scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
     }
 }
 
 #Preview {
-    ScrumsView()
+    ScrumsView(scrums: DailyScrum.sampleData)
 }
